c083ba73df072dac995fc3b9e3ef5bbc9da168dd
MuhannadSatouf <muhannad.satouf0016@stud.hkr.se>
2019-12-04 21:34:21 +0100

    'new changes and adding other methods to the HotelLogic calss

--- src/HotelManagementApplication_Group2/Main.java
+++ src/HotelManagementApplication_Group2/Main.java
@@ -17,21 +17,34 @@ public class Main {
             myApp.menu();
             int choice = input.nextInt();
             if (choice == 1) {
-                hotelMyApp.addNewBookingEmploy();
+                hotelMyApp.addCustomer();
             }
-            if (choice == 2) {
-                hotelMyApp.viwBooking();
+            else if (choice == 2) {
+                hotelMyApp.addBooking();
             }
+            else if (choice ==3){
+                hotelMyApp.viewCustomer();
+            }
+            else if (choice == 4){
+                hotelMyApp.search();
+            }
+            else if (choice == 5){
+                hotelMyApp.cancelBooking();
+            }
+
 
         }
     }
 
     public void menu() {
-        System.out.println("Welcome to the hotel application");
-        System.out.println("1-Add booking ");
-        System.out.println("2-view booking");
-        System.out.println("3-Cancel a booking");
-        System.out.println("Choose: ");
+        System.out.println("Employee");                 // The employee menu, here only for testing our method the menu will finish soon.
+        System.out.println("1> new customer");          // add customer to the system without adding booking
+        System.out.println("2> Add booking");           // adding booking with adding customer
+        System.out.println("3> List of customers");
+        System.out.println("4> Search");
+        System.out.println("5> Cancel booking");
+        System.out.println("6> View all booking");
+        System.out.println("Enter your choice: ");
 
     }
 
